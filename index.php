692b3ac23c83d04e948c8486ecf2c6b39410ff72
root <danielzarazua>
2019-03-19 10:16:56 -0600

    ADD: otro archivo

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Biblioteca de Dan", "\n";
+
+echo "Mis libros favoritos son sobre misterio y suspenso.","\n";
+
+function acres ($metro)
+
+	{
+		$acre = 4046.86;
+		
+		$conversion = $metro * $acre;
+
+
+		return $conversion;
+
+	}	
+		echo (acres(1));
+
+
+?>
